697d1acff3f6bf087e6de4beac3be8aa4b254719
congxuesongzhen <2511936269@qq.com>
2026-06-26 21:05:24 +0800

    3

--- src/config/sponsorConfig.ts
+++ src/config/sponsorConfig.ts
@@ -44,20 +44,5 @@ export const sponsorConfig: SponsorConfig = {
 	// 打赏者列表（可选）
 	sponsors: [
 		// 示例：已实名打赏者
-		{
-			name: "夏叶",
-			avatar:
-				"https://weavatar.com/avatar/d252655d40d6874417a720bad0a6c5f77f8f6a1fd2f882f8f338402dc37e4190?s=640",
-			amount: "¥50",
-			date: "2025-10-01",
-		},
-
-		// 示例：匿名打赏者
-		{
-			name: "匿名用户",
-			// avatar: "",
-			amount: "¥20",
-			date: "2025-10-01",
-		},
 	],
 };
